1b4baa40cf54f25721e5ab59a1fe5bb29eb572ee
ycp998 <ycp998@naver.com>
2019-01-05 05:20:47 -0600

    remove
    rv

--- euler/problem-1[Multiples of 3 and 5].py
+++
@@ -1,8 +0,0 @@
-sum=0 # sum is an integer variable to sum multiple of (3 or 5) below 1000
-
-for x in range(1,1000):
-    if (x%3==0 or x%5==0): # add x if x is multiple of 3 or 5 
-        sum+=x
-
-print(sum)  # print answer (233168) :) 
-
