ec59c9537469d5f08697e0017a0fb39b39b73194
lodado <40421183+lodado@users.noreply.github.com>
2022-01-24 17:47:12 +0900

    Update 234PalindromeLinkedList.py

--- leetcode/링크드리스트/234PalindromeLinkedList.py
+++ leetcode/링크드리스트/234PalindromeLinkedList.py
@@ -4,31 +4,21 @@ class ListNode:
         self.val = val
         self.next = next
         self.prev = None
-        
+
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        header = ListNode(None, None)
-        cursor = header
+        header = head
+        cursor = None
         
         while(head):
-            newList = ListNode(head.val, None)
-            newList.prev = cursor
-            cursor.next = newList
-            cursor = newList
+            head.prev = cursor
+            cursor = head
             head = head.next
-
-        header = header.next
 
         while(header and cursor):
             if(cursor.val != header.val):
                 return False
             header = header.next
             cursor = cursor.prev
-        
         return True
-        
-        
-        
-        
-        
-        
