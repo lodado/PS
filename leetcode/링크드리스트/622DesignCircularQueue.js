22ac2f3685a4a9002b2774bfb809d5b51f6aa15d
lodado <40421183+lodado@users.noreply.github.com>
2022-02-04 17:47:45 +0900

    Update 622DesignCircularQueue.js

--- leetcode/링크드리스트/622DesignCircularQueue.js
+++ leetcode/링크드리스트/622DesignCircularQueue.js
@@ -1,7 +1,7 @@
 /**
  * @param {number} k
  */
-var MyCircularQueue = function(k) {
+const MyCircularQueue = function(k) {
     this.queue = Array.from({length : k}, ()=> null);
     this.front = 0;
     this.back = 0;
@@ -66,14 +66,3 @@ MyCircularQueue.prototype.isEmpty = function() {
 MyCircularQueue.prototype.isFull = function() {
     return this.front === this.back && this.queue[this.front] !== null;
 };
-
-/** 
- * Your MyCircularQueue object will be instantiated and called as such:
- * var obj = new MyCircularQueue(k)
- * var param_1 = obj.enQueue(value)
- * var param_2 = obj.deQueue()
- * var param_3 = obj.Front()
- * var param_4 = obj.Rear()
- * var param_5 = obj.isEmpty()
- * var param_6 = obj.isFull()
- */
