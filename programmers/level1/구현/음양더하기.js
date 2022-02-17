32fecfe2072c9bbd25d05a12b6c357bbb6b6b348
lodado <40421183+lodado@users.noreply.github.com>
2022-02-17 23:54:36 +0900

    Create 음양더하기.js

---
+++ programmers/level1/구현/음양더하기.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+    let sum = 0
+    
+    for(var i=0; i<absolutes.length; i++)
+        sum += signs[i]? absolutes[i] : -absolutes[i]
+        
+    return sum;
+}
