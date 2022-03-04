fc727740161a8b16791cf79408ea3140f27a97b2
lodado <40421183+lodado@users.noreply.github.com>
2022-03-04 22:27:02 +0900

    Create 파일명정렬.py

---
+++ programmers/level2/정규식/파일명정렬.py
@@ -0,0 +1,22 @@
+import re
+
+def sort(inputs):
+    
+    return [inputs[0].upper(), int(inputs[1])]
+
+def solution(files):
+    lists = []
+    answer = []
+    
+    for file in files:
+        num = re.search('([^0-9]+)(\d+)(.+)', file)
+        lists.append(num.groups())
+    lists.sort(key=sort)
+    
+    for head, number, tail in lists:
+        string = head+number
+        if(tail):
+            string+=tail
+        answer.append(string)    
+    
+    return answer
